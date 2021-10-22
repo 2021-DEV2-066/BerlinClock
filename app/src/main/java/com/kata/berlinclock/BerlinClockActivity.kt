c6257c7adb97e7d9ce47177e5529b5113e0e73b3
2021-DEV2-066 <2021dev2066@gmail.com>
2021-10-22 11:16:02 +0530

    code refactor, renamed mainactivity to BerlinClockActivity and renamed its layout file as well

--- app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
+++ app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
@@ -3,9 +3,9 @@ package com.kata.berlinclock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BerlinClockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_berlin_clock)
     }
 }
