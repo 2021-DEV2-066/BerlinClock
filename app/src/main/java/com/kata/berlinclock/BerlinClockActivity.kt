288a166bf2d0b4009d1a86b55e1fa4089928882e
2021-DEV2-066 <2021dev2066@gmail.com>
2021-10-22 11:21:50 +0530

    designed berlin clock activity and added required colors and drawables

--- app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
+++ app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
@@ -2,10 +2,20 @@ package com.kata.berlinclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kata.berlinclock.databinding.ActivityBerlinClockBinding
 
 class BerlinClockActivity : AppCompatActivity() {
+    private var _binding: ActivityBerlinClockBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_berlin_clock)
+        _binding = ActivityBerlinClockBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
