b1b866567b1b5076c91f15692c1608caca522a97
2021-DEV2-066 <2021dev2066@gmail.com>
2021-10-22 11:38:49 +0530

    injected viewmodel in activity and added code to observe live data

--- app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
+++ app/src/main/java/com/kata/berlinclock/BerlinClockActivity.kt
@@ -3,15 +3,26 @@ package com.kata.berlinclock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kata.berlinclock.databinding.ActivityBerlinClockBinding
+import com.kata.berlinclock.viewmodel.BerlinClockViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BerlinClockActivity : AppCompatActivity() {
     private var _binding: ActivityBerlinClockBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: BerlinClockViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityBerlinClockBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.berlinClockTime.observe(this) {
+
+        }
     }
 
     override fun onDestroy() {
