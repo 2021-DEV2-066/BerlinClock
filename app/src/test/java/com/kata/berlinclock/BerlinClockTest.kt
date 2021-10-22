5ff5980288c1d5031cde909e96964fa8feb52c56
2021-DEV2-066 <2021dev2066@gmail.com>
2021-10-22 10:00:48 +0530

    test case for 17 minutes

--- app/src/test/java/com/kata/berlinclock/BerlinClockTest.kt
+++ app/src/test/java/com/kata/berlinclock/BerlinClockTest.kt
@@ -158,4 +158,14 @@ class BerlinClockTest {
         val expectedResult = Minutes(topColors = minutesOnTop, bottomColors = minutesOnBottom)
         assertThat(expectedResult).isEqualTo(result)
     }
+
+    @Test
+    fun test_17_minutes() {
+        val result = berlinClock.getMinutes(17)
+
+        val minutesOnTop = listOf(YELLOW, YELLOW, RED, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF)
+        val minutesOnBottom = listOf(YELLOW, YELLOW, OFF, OFF)
+        val expectedResult = Minutes(topColors = minutesOnTop, bottomColors = minutesOnBottom)
+        assertThat(expectedResult).isEqualTo(result)
+    }
 }
