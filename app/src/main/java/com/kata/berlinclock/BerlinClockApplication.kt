bfc4eb4f617a29482a89164759ce3417a6f31e02
2021-DEV2-066 <2021dev2066@gmail.com>
2021-10-22 11:05:08 +0530

    created berlin clock viewmodel and configured the same in koin

--- app/src/main/java/com/kata/berlinclock/BerlinClockApplication.kt
+++ app/src/main/java/com/kata/berlinclock/BerlinClockApplication.kt
@@ -1,6 +1,8 @@
 package com.kata.berlinclock
 
 import android.app.Application
+import com.kata.berlinclock.viewmodel.BerlinClockViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -8,11 +10,15 @@ class BerlinClockApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(listOf(appModule))
+            modules(listOf(appModule, viewModelModule))
         }
     }
 }
 
 val appModule = module {
     single { BerlinClock() }
+}
+
+val viewModelModule = module {
+    viewModel { BerlinClockViewModel(get()) }
 }
---
+++ app/src/main/java/com/kata/berlinclock/viewmodel/BerlinClockViewModel.kt
@@ -0,0 +1,7 @@
+package com.kata.berlinclock.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.kata.berlinclock.BerlinClock
+
+class BerlinClockViewModel(private val berlinClock: BerlinClock) : ViewModel() {
+}
